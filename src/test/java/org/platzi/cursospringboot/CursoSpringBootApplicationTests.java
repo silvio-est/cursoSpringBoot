b9fe467d312459fbed0f31a922c31add8dd64793
Silvio Estevan Montalvo <msilvioesteban@gmail.com>
2024-01-17 16:26:46 -0500

    first commit

---
+++ src/test/java/org/platzi/cursospringboot/CursoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.platzi.cursospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
